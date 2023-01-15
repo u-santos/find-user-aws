ada3008aa48f4bb932a8200f43e4e700eea57d41
Ulisses Oliveira <ulisses.oliveira@sambatech.com.br>
2023-01-15 15:18:22 -0300

    Feature: List user policies

--- keysearch.py
+++ keysearch.py
@@ -14,13 +14,14 @@ for line in myfile:
 
         print("\nSearching for KEY on SSO: {} account...".format(env))
         for user in iam.list_users()['Users']:
-            for response in paginator.paginate(UserName=user['UserName']):
+            user_name = user['UserName']
+            for response in paginator.paginate(UserName=user_name):
                 if len(response['AccessKeyMetadata']) > 0:
                     access_key_metadata = response['AccessKeyMetadata'][0]
                     if KEY == access_key_metadata['AccessKeyId']:
                         search = True
-                        inline_user_policies = iam.list_user_policies(UserName=user['UserName'])
-                        managed_user_policies = iam.list_attached_user_policies(UserName=user['UserName'])
+                        inline_user_policies = iam.list_user_policies(UserName=user_name)
+                        managed_user_policies = iam.list_attached_user_policies(UserName=user_name)
                         
                         print("\nKey found!\n \n{} in {} user. \nSSO: {}".format(sys.argv[1], access_key_metadata['UserName'], env))
                         print("\nInline Policies: {}".format(inline_user_policies))
