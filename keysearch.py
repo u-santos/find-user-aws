37bac7d6cb0512ff58aa1d8e6967ec253d2e47e5
Ulisses Oliveira <ulisses.oliveira@sambatech.com.br>
2023-01-15 20:59:09 -0300

    Feature: Create IAM User

--- keysearch.py
+++ keysearch.py
@@ -4,6 +4,7 @@ import sys
 myfile = open("/home/ulisses/.aws/credentials", "r")
 KEY = sys.argv[1]
 search = False
+
 for line in myfile:
     if line[0] == "[" and line[len(line) - 2]  == "]":
         env = line[1:(len(line) - 2)]
@@ -23,9 +24,23 @@ for line in myfile:
                         inline_user_policies = iam.list_user_policies(UserName=user_name)
                         managed_user_policies = iam.list_attached_user_policies(UserName=user_name)
                         
-                        print("\nKey found!\n \n{} in {} user. \nSSO: {}".format(sys.argv[1], access_key_metadata['UserName'], env))
-                        print("\nInline Policies: {}".format(inline_user_policies))
-                        print("\nManaged Policies: {}".format(managed_user_policies))
+                        print("\nKey found!\n \n{} in {} user. \nSSO: {}".format(KEY, user_name, env))
+                        print("\nInline Policies:\n{}".format(inline_user_policies))
+                        print("\nManaged Policies:\n{}".format(managed_user_policies))
+                        print(" ")
+
+                        try:
+                            new_user_name = "TEST-{}".format(user_name)
+                            iam.create_user(UserName=new_user_name)
+                            for policies in managed_user_policies['AttachedPolicies']:
+                                iam.attach_user_policy(UserName=new_user_name, PolicyArn=policies['PolicyArn'])
+                            access_key_pair = user.create_access_key_pair()
+                            print("\nUser Created!")
+                            for response in paginator.paginate(UserName=new_user_name):
+                                print(response)
+                        except Exception as e:
+                            print("Failed: Error on create a new user, please contact your administrator.")
+                            print(e)
     if search:
         break
 myfile.close()
