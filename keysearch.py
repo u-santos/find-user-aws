abe3945707a3460e526f77d2d2fe0e956300330b
Ulisses Oliveira <ulisses.oliveira@sambatech.com.br>
2023-01-16 10:55:42 -0300

    Feature: Remove old prints, add creation of secret and access key

--- keysearch.py
+++ keysearch.py
@@ -11,6 +11,7 @@ for line in myfile:
         session = boto3.Session(profile_name=env)
 
         iam = session.client('iam')
+        iam_resource = session.resource('iam')
         paginator = iam.get_paginator('list_access_keys')
 
         print("\nSearching for KEY on SSO: {} account...".format(env))
@@ -25,21 +26,22 @@ for line in myfile:
                         managed_user_policies = iam.list_attached_user_policies(UserName=user_name)
                         
                         print("\nKey found!\n \n{} in {} user. \nSSO: {}".format(KEY, user_name, env))
-                        print("\nInline Policies:\n{}".format(inline_user_policies))
-                        print("\nManaged Policies:\n{}".format(managed_user_policies))
-                        print(" ")
 
                         try:
                             new_user_name = "TEST-{}".format(user_name)
                             iam.create_user(UserName=new_user_name)
                             for policies in managed_user_policies['AttachedPolicies']:
                                 iam.attach_user_policy(UserName=new_user_name, PolicyArn=policies['PolicyArn'])
-                            access_key_pair = user.create_access_key_pair()
+                            new_user_key = iam_resource.User(new_user_name)
+                            access_key_pair = new_user_key.create_access_key_pair()
+                            print('\nNEW KEY GENERATED - PLEASE RECORD THE NEW KEYS')
+                            print('THEY WILL NOT BE DISPLAYED AGAIN')
+                            print('\nid:', access_key_pair.access_key_id)
+                            print('secret:', access_key_pair.secret)
+                            print('status:', access_key_pair.status)
                             print("\nUser Created!")
-                            for response in paginator.paginate(UserName=new_user_name):
-                                print(response)
                         except Exception as e:
-                            print("Failed: Error on create a new user, please contact your administrator.")
+                            print("Failed: Error to create new user, please contact your administrator.")
                             print(e)
     if search:
         break
